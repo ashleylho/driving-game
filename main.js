fd5e60f1f85f0996a4ccd8701fcee23320cf16a2
Ashley Ho <ashley.lho@gmail.com>
2022-09-21 19:35:51 +0000

    listened for keydown events on the document to turn the car in the direction of the key pressed. #2

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 var data = {
   direction: 'east'
 };
+
+var $img = document.querySelector('img');
+document.addEventListener('keydown', handleKeydown);
+
+function handleKeydown(event) {
+  if (event.key === 'ArrowUp') {
+    data.direction = 'north';
+    $img.className = 'north';
+  } else if (event.key === 'ArrowDown') {
+    data.direction = 'south';
+    $img.className = 'south';
+  } else if (event.key === 'ArrowRight') {
+    data.direction = 'east';
+    $img.className = 'east';
+  } else if (event.key === 'ArrowLeft') {
+    data.direction = 'west';
+    $img.className = 'west';
+  }
+}
